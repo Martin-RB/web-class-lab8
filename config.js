620b5930385193b4ede249e9bd5b5ed32484f5b8
Martin Rivas <m.rivas@denlin.com.mx>
2019-11-17 14:45:51 -0600

    Aceptalo porfa x3

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://martin:Marter#1665142@cluster0-kegpb.mongodb.net/martin-blog-post?retryWrites=true&w=majority";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/martin-blog-post";
 exports.PORT = process.env.PORT || 6969;
